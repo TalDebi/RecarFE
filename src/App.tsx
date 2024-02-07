d13740906213082abff9fa9d215f9b8ce8f72cef
טל דבי <taldebi2@gmail.com>
2024-02-07 19:59:33 +0200

    added recar logo

--- src/App.tsx
+++ src/App.tsx
@@ -44,14 +44,16 @@
 //   )
 // }
 
+import Navbar from "./Navbar"
 import PostList from "./components/PostsList"
 import Registration from "./components/Registration"
 
 function App() {
   return (
-    <div className="p-2">
-      <Registration />
-      <PostList />
+    <div>
+      <Navbar/>
+      {/* <Registration />
+      <PostList /> */}
     </div>
   )
 }
