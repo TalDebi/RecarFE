69f9ee026aefd9bd01e5d50d7c9d62d106549537
טל דבי <taldebi2@gmail.com>
2024-02-13 20:39:30 +0200

    added search page top bar

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  useTheme,
+  IconButton,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Search() {
-    const pageIndex = 2;
+  const theme = useTheme();
 
-    return (
-        <div>
-            {pageIndex}
-        </div>
-    )}
+  return (
+    <Container
+      sx={{
+        position: "fixed",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100%",
+        minWidth: "100%",
+        padding: 4,
+        backgroundColor: theme.palette.secondary.light,
+      }}
+    >
+      <Box width={1000}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <TextField
+            id="searchInput"
+            variant="outlined"
+            sx={{ backgroundColor: "white", width: "100%", marginRight: 2 }}
+          />
+          <Box display="flex">
+            <Button
+              variant="contained"
+              sx={{ height: "100%", boxShadow: "none" }}
+            >
+              <IconButton>
+                <SearchIcon sx={{ color: theme.palette.secondary.light }} />
+              </IconButton>
+            </Button>
+            <Button variant="text" sx={{ color: theme.palette.primary.dark }}>
+              נקה
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
 
-export default Search
+export default Search;
