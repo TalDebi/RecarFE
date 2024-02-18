d56703075040d1b6306593afdd0bf41139112c4a
טל דבי <taldebi2@gmail.com>
2024-02-19 00:20:27 +0200

    added mui and react carousel

--- src/pages/Car/Car.tsx
+++ src/pages/Car/Car.tsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import carImage from "../../assets/toyotaExample.avif";
 import { Container, useTheme } from "@mui/material";
 import { useParams } from "react-router";
+import Carousel from "react-material-ui-carousel";
 
 function ResultsTable() {
   const theme = useTheme();
@@ -22,7 +23,22 @@ function ResultsTable() {
         overflow: "auto",
       }}
     >
-      <Box>{carID}</Box>
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ width: 400, height: 400 }}>
+          <Carousel>
+            {[carImage, carImage, carImage, carImage, carImage, carImage].map(
+              (image, index: number): JSX.Element => (
+                <img
+                  key={index}
+                  src={image}
+                  alt="no image"
+                  style={{ width: "100%" }}
+                />
+              )
+            )}
+          </Carousel>
+        </Box>
+      </Box>
     </Container>
   );
 }
