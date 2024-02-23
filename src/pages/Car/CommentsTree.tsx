7f0063799ac726cdbc9999971c26cf00f5d6b3a4
טל דבי <taldebi2@gmail.com>
2024-02-23 15:59:23 +0200

    styled tree item

--- src/pages/Car/CommentsTree.tsx
+++ src/pages/Car/CommentsTree.tsx
@@ -2,38 +2,22 @@ import * as React from "react";
 import { SxProps, Theme, styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import MailIcon from "@mui/icons-material/Mail";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Label from "@mui/icons-material/Label";
-import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import InfoIcon from "@mui/icons-material/Info";
-import ForumIcon from "@mui/icons-material/Forum";
-import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import { SvgIconProps } from "@mui/material/SvgIcon";
 import { TreeView } from "@mui/x-tree-view/TreeView";
 import {
   TreeItem,
   TreeItemProps,
   treeItemClasses,
 } from "@mui/x-tree-view/TreeItem";
-
-declare module "react" {
-  interface CSSProperties {
-    "--tree-view-color"?: string;
-    "--tree-view-bg-color"?: string;
-  }
-}
+import { Avatar, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 type StyledTreeItemProps = TreeItemProps & {
-  bgColor?: string;
-  bgColorForDarkMode?: string;
-  color?: string;
-  colorForDarkMode?: string;
-  labelIcon: React.ElementType<SvgIconProps>;
-  labelInfo?: string;
-  labelText: string;
+  commentText: string;
+  username: string;
+  avatarUrl?: string;
 };
 
 const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
@@ -62,7 +46,7 @@ const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
   [`& .${treeItemClasses.group}`]: {
     marginLeft: 0,
     [`& .${treeItemClasses.content}`]: {
-      paddingLeft: theme.spacing(2),
+      paddingLeft: theme.spacing(6),
     },
   },
 })) as unknown as typeof TreeItem;
@@ -72,23 +56,7 @@ const StyledTreeItem = React.forwardRef(function StyledTreeItem(
   ref: React.Ref<HTMLLIElement>
 ) {
   const theme = useTheme();
-  const {
-    bgColor,
-    color,
-    labelIcon: LabelIcon,
-    labelInfo,
-    labelText,
-    colorForDarkMode,
-    bgColorForDarkMode,
-    ...other
-  } = props;
-
-  const styleProps = {
-    "--tree-view-color":
-      theme.palette.mode !== "dark" ? color : colorForDarkMode,
-    "--tree-view-bg-color":
-      theme.palette.mode !== "dark" ? bgColor : bgColorForDarkMode,
-  };
+  const { avatarUrl, commentText, username, ...other } = props;
 
   return (
     <StyledTreeItemRoot
@@ -97,23 +65,38 @@ const StyledTreeItem = React.forwardRef(function StyledTreeItem(
           sx={{
             display: "flex",
             alignItems: "center",
+            justifyContent: "space-between",
             p: 0.5,
             pr: 0,
           }}
         >
-          <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
-          <Typography
-            variant="body2"
-            sx={{ fontWeight: "inherit", flexGrow: 1 }}
-          >
-            {labelText}
-          </Typography>
-          <Typography variant="caption" color="inherit">
-            {labelInfo}
-          </Typography>
+          <Box sx={{ display: "flex" }}>
+            <Avatar src={avatarUrl} alt="Remy Sharp" sx={{ mr: 1 }} />
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: "inherit", flexGrow: 1 }}
+              >
+                {username}
+              </Typography>
+              <Typography
+                variant="caption"
+                sx={{ fontWeight: "inherit", flexGrow: 1 }}
+              >
+                {commentText}
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: "flex" }}>
+            <IconButton size="small" aria-label="editIcon">
+              <EditIcon />
+            </IconButton>
+            <IconButton size="small" aria-label="deleteIcon">
+              <DeleteIcon />
+            </IconButton>
+          </Box>
         </Box>
       }
-      style={styleProps}
       {...other}
       ref={ref}
     />
@@ -133,58 +116,25 @@ export default function CommentsTree({ style }: CommentsTreeProps) {
       defaultExpandIcon={<ArrowRightIcon />}
       defaultEndIcon={<div style={{ width: 24 }} />}
       sx={{
-        height: 264,
         flexGrow: 1,
-        maxWidth: 400,
         overflowY: "auto",
         ...style,
       }}
     >
-      <StyledTreeItem nodeId="1" labelText="All Mail" labelIcon={MailIcon} />
-      <StyledTreeItem nodeId="2" labelText="Trash" labelIcon={DeleteIcon} />
-      <StyledTreeItem nodeId="3" labelText="Categories" labelIcon={Label}>
-        <StyledTreeItem
-          nodeId="5"
-          labelText="Social"
-          labelIcon={SupervisorAccountIcon}
-          labelInfo="90"
-          color="#1a73e8"
-          bgColor="#e8f0fe"
-          colorForDarkMode="#B8E7FB"
-          bgColorForDarkMode="#071318"
-        />
-        <StyledTreeItem
-          nodeId="6"
-          labelText="Updates"
-          labelIcon={InfoIcon}
-          labelInfo="2,294"
-          color="#e3742f"
-          bgColor="#fcefe3"
-          colorForDarkMode="#FFE2B7"
-          bgColorForDarkMode="#191207"
-        />
-        <StyledTreeItem
-          nodeId="7"
-          labelText="Forums"
-          labelIcon={ForumIcon}
-          labelInfo="3,566"
-          color="#a250f5"
-          bgColor="#f3e8fd"
-          colorForDarkMode="#D9B8FB"
-          bgColorForDarkMode="#100719"
-        />
-        <StyledTreeItem
-          nodeId="8"
-          labelText="Promotions"
-          labelIcon={LocalOfferIcon}
-          labelInfo="733"
-          color="#3c8039"
-          bgColor="#e6f4ea"
-          colorForDarkMode="#CCE8CD"
-          bgColorForDarkMode="#0C130D"
-        />
+      <StyledTreeItem
+        nodeId="1"
+        commentText="האם הרכב עבר תאונה?"
+        username="טל"
+        avatarUrl="/static/images/avatar/2.jpg"
+      />
+      <StyledTreeItem nodeId="2" commentText="Trash" username="טל" />
+      <StyledTreeItem nodeId="3" commentText="Categories" username="טל">
+        <StyledTreeItem nodeId="5" commentText="Social" username="טל" />
+        <StyledTreeItem nodeId="6" commentText="Updates" username="טל" />
+        <StyledTreeItem nodeId="7" commentText="Forums" username="טל" />
+        <StyledTreeItem nodeId="8" commentText="Promotions" username="טל" />
       </StyledTreeItem>
-      <StyledTreeItem nodeId="4" labelText="History" labelIcon={Label} />
+      <StyledTreeItem nodeId="4" commentText="History" username="טל" />
     </TreeView>
   );
 }
