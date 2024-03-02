455d8b3e5af6faad47f58ceb6d90610ce0fe4ef7
טל דבי <taldebi2@gmail.com>
2024-03-03 01:37:23 +0200

    finished my cars layout

--- src/pages/MyCars/CarInfoCard.tsx
+++ src/pages/MyCars/CarInfoCard.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { MouseEvent } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -6,22 +6,49 @@ import CardMedia from "@mui/material/CardMedia";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import carImage from "../../assets/toyotaExample.avif";
-import { Badge } from "@mui/material";
+import { ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material";
 import MoreVert from "@mui/icons-material/MoreVert";
-import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+import FileOpenIcon from "@mui/icons-material/FileOpen";
+import { useNavigate } from "react-router-dom";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 const mainInfo = ["יצרן", "דגם", "שנה", "איזור מכירה"];
 
-function CarInfoCard() {
-  const handleOpenMoreOptions = (): void => {
-    console.log(1);
+interface CarInfoCardProps {
+  postId: string;
+}
+
+function CarInfoCard({ postId }: CarInfoCardProps) {
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleNavigateToPost = (): void => {
+    navigate(`../Car/${postId}`, { replace: true });
   };
 
+  const handleOpenMoreOptions = (
+    event: MouseEvent<HTMLButtonElement>
+  ): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMoreOptions = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenInNewTab = () => {
+    window.open(`${window.location.origin}/Car/${postId}`, "_blank");
+    handleCloseMoreOptions();
+  };
+
+  const moreOptions = [{ label: "פתח בחלון חדש", action: handleOpenInNewTab }];
+
   return (
-    <Card sx={{ display: "flex", width: 1200, height: 155, mb: 2 }}>
+    <Card sx={{ display: "flex", width: "100%", height: 155, mb: 2 }}>
       <CardMedia
         component="img"
-        sx={{ width: 250, height: 160 }}
+        sx={{ width: 350, height: 160 }}
         image={carImage}
         alt="no image provided"
       />
@@ -61,20 +88,42 @@ function CarInfoCard() {
           <IconButton
             sx={{ mr: 2 }}
             size="large"
-            aria-label="show 17 new notifications"
             color="inherit"
+            onClick={handleNavigateToPost}
           >
-            <Badge badgeContent={17} color="error">
-              <CircleNotificationsIcon />
-            </Badge>
+            <FileOpenIcon />
           </IconButton>
-          <IconButton
-            size="large"
-            onClick={handleOpenMoreOptions}
-            color="inherit"
-          >
-            <MoreVert />
-          </IconButton>
+          <Box>
+            <IconButton
+              id="moreOptionsbutton"
+              aria-controls={open ? "menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              size="large"
+              onClick={handleOpenMoreOptions}
+              color="inherit"
+            >
+              <MoreVert />
+            </IconButton>
+            <Menu
+              id="menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleCloseMoreOptions}
+              MenuListProps={{
+                "aria-labelledby": "moreOptionsbutton",
+              }}
+            >
+              {moreOptions.map((option) => (
+                <MenuItem onClick={option.action}>
+                  <ListItemText>{option.label}</ListItemText>
+                  <ListItemIcon sx={{ ml: 1 }}>
+                    <OpenInNewIcon fontSize="small" />
+                  </ListItemIcon>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Box>
       </CardContent>
     </Card>
--- src/pages/MyCars/MyCars.tsx
+++ src/pages/MyCars/MyCars.tsx
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
 import { useTheme } from "@mui/material/styles";
-import { Fab } from "@mui/material";
+import { Box, Button, Tab, Tabs, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import CarInfoCard from "./CarInfoCard";
 import RecarDialog from "../../customComponents/RecarDialog";
 import CarInfoForm from "../CarInfoForm";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Box
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 function MyCars() {
   const theme = useTheme();
@@ -14,16 +47,55 @@ function MyCars() {
     setOpen(!isOpen);
   };
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-      {[1, 2, 3].map(
-        (index: number): JSX.Element => (
-          <CarInfoCard key={index} />
-        )
-      )}
-      <Fab color="primary" aria-label="add" onClick={handleCreatePost}>
-        <AddIcon />
-      </Fab>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          width: 1400,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="הפוסטים שלי" {...a11yProps(0)} />
+          <Tab label="פוסטים שאהבתי" {...a11yProps(1)} />
+        </Tabs>
+        <Button
+          component="label"
+          variant="contained"
+          tabIndex={-1}
+          aria-label="add"
+          onClick={handleCreatePost}
+          endIcon={<AddIcon />}
+          sx={{ height: "fit-content", mt: 0.5 }}
+        >
+          פוסט חדש
+        </Button>
+      </Box>
+      <Box sx={{ overflow: "auto", height: 500 }}>
+        <CustomTabPanel value={value} index={0}>
+          {[1, 2].map(
+            (index: number): JSX.Element => (
+              <CarInfoCard key={index} postId={"1"} />
+            )
+          )}
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          {[1, 2, 3, 4].map(
+            (index: number): JSX.Element => (
+              <CarInfoCard key={index} postId={"2"} />
+            )
+          )}
+        </CustomTabPanel>
+      </Box>
       <RecarDialog
         open={isOpen}
         setOpen={setOpen}
