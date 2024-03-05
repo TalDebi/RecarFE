4a2d7c51dfd4f81ef8448dbbf9f63ece0e91f5f3
Nir <nirkopp@gmail.com>
2024-03-05 23:14:54 +0200

    sda

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = dirname(__filename);
 const app = expressApp();
-const PORT = 3000;
+const PORT = 80;
 
 
 app.use(expressApp.static(path.join(__dirname, 'dist')));
